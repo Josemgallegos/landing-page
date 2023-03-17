e5177374b698fe349b7410681d3471b60cd65cde
cuevas <juan.m.cuevas@outlook.com>
2023-03-17 11:27:36 -0600

    Modify timeline h2 color

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+//Almacenar el primer elemento con la clase ".menu"
 const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.menu-btn');
 
+//Modificar clase de la variable menuBtn por medio de una arrow function al hacer click sobre el elemento
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu-open');
 });
